895a8e9d1f10231fbc8bd58cf8680f4cf3fbbf68
Morten Sjøgren <m_abs@mabs.dk>
2016-03-01 21:53:03 +0100

    Match player URL

--- app/main-page.ts
+++ app/main-page.ts
@@ -1,16 +1,35 @@
 import view = require("ui/core/view");
 
-import {MyWebView} from './xml-declaration/my-web-view';
+import {MyWebView} from './xml-declaration/my-web-view/my-web-view';
+
+import {openUrl} from 'utils/utils';
 
 let webView: MyWebView;
+
+const playerRegExp = /m\.e17\.dk\/embedded\/#book-player\?book\=([0-9a-z]*)/i;
 
 export function pageLoaded(args) {
     var page = args.object;
 
     webView = <MyWebView>view.getViewById(page, 'webview');
     webView.urlOverrideHandler = (url: string) => {
-      let res = url.match(/\bmadforandrer\.dk/i);
+      const matchPlayer = url.match(playerRegExp);
+      if (matchPlayer) {
+        console.log(`player_url: ${url}`, matchPlayer);
 
-      return !res;
+        return true;
+      }
+
+      const external = !url.match(/\bnota\.dk/i);
+      if (external) {
+        console.log(`external: ${url}`);
+        openUrl(url);
+
+        return true;
+      }
+
+      console.log('internal:', url);
+
+      return false;
     };
 }
